b85d5f37ae6f051c786b85b74d817edcb3448ab4
Derek Anderson <derek.anderson@walmartlabs.com>
2021-01-28 12:17:25 -0600

    run pretty

--- test/spec/load-config.spec.js
+++ test/spec/load-config.spec.js
@@ -1,21 +1,20 @@
-const { loadConfig } = require("../../lib");
+const { loadConfig } = require("../..");
 const path = require("path");
 const { expect } = require("chai");
 
-
 describe("confippet loadConfig", function () {
-    it("should compose config from directory", function () {
-        const config = loadConfig(
-            {
-                dirs: [path.join(__dirname, "../config1")],
-                context: {
-                  deployment: "default"
-                }
-            },
-            { foo: "bar" }
-        );
+  it("should compose config from directory", function () {
+    const config = loadConfig(
+      {
+        dirs: [path.join(__dirname, "../config1")],
+        context: {
+          deployment: "default",
+        },
+      },
+      { foo: "bar" }
+    );
 
-        expect(config).to.have.property("foo");
-        expect(config.js).to.equal("1");
-    });
+    expect(config).to.have.property("foo");
+    expect(config.js).to.equal("1");
+  });
 });
