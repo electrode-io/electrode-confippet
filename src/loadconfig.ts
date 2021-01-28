04d85cf8d9307cab5edc731942cc2f5ab6e10a71
Derek Anderson <derek.anderson@walmartlabs.com>
2021-01-28 14:28:50 -0600

    cache spec unless asked to reload.

--- src/loadconfig.ts
+++ src/loadconfig.ts
@@ -1,12 +1,29 @@
 import store from "./store";
-import presetConfig from "./preset-config";
 
-const config = store();
+const configs = {};
 
-export = (composeOptions, defaults) => {
+const uniqueKey = (stringArray: any) => {
+  return stringArray.join("");
+};
 
+export = (composeOptions, defaults, refresh) => {
+  const cacheKey = uniqueKey(composeOptions.dirs);
+  let config = configs[cacheKey];
+
+  if (!config) {
+    config = store();
+  }
+
+  if (refresh) {
+    config._$.reset();
+  }
+
+  if (!configs[cacheKey] || refresh) {
+    if (!refresh) config._cached = true; //this ok?
     config._$.defaults(defaults);
     config._$.compose(composeOptions);
+    configs[cacheKey] = config;
+  }
 
-    return config;
+  return config;
 };
--- test/spec/load-config.spec.js
+++ test/spec/load-config.spec.js
@@ -17,4 +17,37 @@ describe("confippet loadConfig", function () {
     expect(config).to.have.property("foo");
     expect(config.js).to.equal("1");
   });
+
+  it("should hand back cached config", function () {
+    const config = loadConfig(
+      {
+        dirs: [path.join(__dirname, "../config1")],
+        context: {
+          deployment: "default",
+        },
+      },
+      { foo: "bar2" }
+    );
+
+    expect(config).to.have.property("_cached");
+    expect(config).to.have.property("foo");
+    expect(config.foo).to.equal("bar");
+    expect(config.js).to.equal("1");
+  });
+
+  it("should reload cached config", function () {
+    const config = loadConfig(
+      {
+        dirs: [path.join(__dirname, "../config1")],
+        context: {
+          deployment: "default",
+        },
+      },
+      { foo: "bar2" },
+      true
+    );
+    expect(config).to.have.property("foo");
+    expect(config.foo).to.equal("bar2");
+    expect(config).to.not.have.property("_cached");
+  });
 });
