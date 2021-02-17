055830e75a8abef1cbe29eb846af7fc6a6109d96
Joel Chen <xchen@walmartlabs.com>
2021-02-17 09:37:56 -0800

    allow loading config and by pass cache

--- test/spec/load-config.spec.ts
+++ test/spec/load-config.spec.ts
@@ -29,7 +29,8 @@ describe("confippet loadConfig", function () {
       { foo: "bar2" }
     );
 
-    // expect(config).to.have.property("_cached");
+    const symbols = Object.getOwnPropertySymbols(config);
+    expect(symbols.map((x) => x.toString())).includes(`Symbol(confippet loadConfig cached config)`);
     expect(config).to.have.property("foo");
     expect(config.foo).to.equal("bar");
     expect(config.js).to.equal("1");
@@ -48,7 +49,25 @@ describe("confippet loadConfig", function () {
     );
     expect(config).to.have.property("foo");
     expect(config.foo).to.equal("bar2");
-    expect(config).to.not.have.property("_cached");
+  });
+
+  it("should allow loading new copy w/o cache", () => {
+    const config: any = loadConfig(
+      {
+        dir: Path.join(__dirname, "../config1"),
+        context: {
+          deployment: "default",
+        },
+        cache: false,
+      },
+      { foo: "bar2" }
+    );
+    const symbols = Object.getOwnPropertySymbols(config);
+    expect(symbols.map((x) => x.toString())).not.includes(
+      `Symbol(confippet loadConfig cached config)`
+    );
+    expect(config).to.have.property("foo");
+    expect(config.foo).to.equal("bar2");
   });
 
   it("should load config from ES6 modules", () => {
